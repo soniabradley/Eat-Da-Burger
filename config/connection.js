3292e69911489c2be9d19697577662de98baafc2
soniab <sonia.bradley524@gmail.com>
2017-10-09 11:36:36 -0500

    deploy

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,17 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+    } else {
+connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "MyNewPass",
   database: "burger_db"
 });
-
+    };
 // Make connection.
 connection.connect(function(err) {
   if (err) {
