e7627a7be8d09ceb6aae6a46ba0b45c8e23b6f4d
soniab <sonia.bradley524@gmail.com>
2018-09-11 20:52:43 -0500

    connect back to Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,18 @@
-// Set up MySQL connection.
-var mysql = require("mysql");
-// var connection;
+// Set up MySQL connection. (HMK)
 
-// if (process.env.JAWSDB_URL) {
-//     connection = mysql.createConnection(process.env.JAWSDB_URL);
-//     } else {
- var connection = mysql.createConnection({
-  port: 3306,
+var mysql = require("mysql");
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+    } else {
+connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "MyNewPass",
   database: "burger_db"
 });
-    // };
+    };
 // Make connection.
 connection.connect(function(err) {
   if (err) {
